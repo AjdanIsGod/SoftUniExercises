621966c9d22a6cda193cd1546014bad2279aa535
AjdanIsGod <AjdanIsGod@users.noreply.github.com>
2021-01-13 19:49:46 +0200

    Update on the exercises.

---
+++ Array_Exercise_MaxSequenceEqual_07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace Array_Exercise_MaxSequenceEqual_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int counter = 0; //This variable is the sequence(number of elements equal to each other). 
+            int winningCounter = 0; //This variable is the max sequence.
+            string number = ""; //Max sequence is stored under this string value.
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i]==arr[i+1]) //If the current element is equal to the next element , we "add" a value to the sequence.
+                {
+                    counter++;
+                    if (counter>winningCounter) //If the first sequence is larger than the max sequence , the max sequence gets the value of the first sequence.
+                    {
+                        winningCounter = counter;
+                        number = arr[i].ToString();
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+            for (int i = 0; i <= winningCounter; i++)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
---
+++ Array_Exercises_EqualSums_06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace Array_Exercises_EqualSums_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (arr.Length==1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            int leftSum=0;
+            int rightSum=0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                leftSum = arr.Take(i).Sum(); //Take gets a given amount of elements from the array from the first element outwards.
+                rightSum = arr.Skip(i + 1).Sum(); //Skip gets the elements after a given index in the array.
+                if (leftSum==rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
