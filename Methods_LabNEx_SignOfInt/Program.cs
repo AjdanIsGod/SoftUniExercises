90b5de80a0bddd6faedc571fc6ea4ca27cfe6bf1
AjdanIsGod <AjdanIsGod@users.noreply.github.com>
2021-02-16 13:10:29 +0200

    Methods lab and exercise

---
+++ Methods_LabNEx_SignOfInt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methods_LabNEx_SignOfInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            solve(a);
+        }
+
+        private static void solve(int a)
+        {
+            if (a>0)
+            {
+                Console.WriteLine($"The number {a} is positive.");
+            }
+            else if(a<0)
+            {
+                Console.WriteLine($"The number {a} is negative.");
+            }
+            else if (a==0)
+            {
+                Console.WriteLine($"The number {a} is zero.");
+            }
+        }
+    }
+}
