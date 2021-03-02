d868d94d85ab4443b0b5bba7bfb53f79cfc13fb8
AjdanIsGod <AjdanIsGod@users.noreply.github.com>
2021-03-02 18:00:25 +0200

    Method exercises

---
+++ Methods_LabNEx_Calculations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Methods_LabNEx_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Solve(text,a,b);
+        }
+
+        private static void Solve(string a, int b , int c)
+        {
+            if (a == "add")
+            {
+                Console.WriteLine(b+c);
+            }
+            else if (a == "multiply") 
+            {
+                Console.WriteLine(b*c);
+            }
+            else if (a=="subtract")
+            {
+                Console.WriteLine(b-c);
+            }
+            else if (a=="divide")
+            {
+                Console.WriteLine(b/c);
+            }
+        }
+    }
+}
---
+++ Methods_LabNEx_PrintTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methods_LabNEx_PrintTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= k; i++)
+            {
+                Solve(1, i);
+            }
+            for (int i = k-1; i >= 1; i--)
+            {
+                Solve(1, i);
+            }
+        }
+
+        private static void Solve(int start,int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
