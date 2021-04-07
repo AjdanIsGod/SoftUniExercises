feba047f4b46e87b13c5f03b341568e0aa1138e5
AjdanIsGod <AjdanIsGod@users.noreply.github.com>
2021-04-07 14:01:23 +0300

    Last exercises of Methods Lab

--- Methods_LabNEx_EvenByOdd/Program.cs
+++ Methods_LabNEx_EvenByOdd/Program.cs
@@ -6,7 +6,51 @@ namespace Methods_LabNEx_EvenByOdd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            
+            int result=GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+
+        private static int GetSumOfOddDigits(int num)
+        {
+            int oddSum = 0;
+
+            while (num > 0)
+            {
+                int lastDigit = num % 10;
+                num = num / 10;
+
+                if (lastDigit % 2 != 0)
+                {
+                    oddSum += lastDigit;
+                }
+            }
+            return oddSum;
+        }
+
+        private static int GetSumOfEvenDigits(int num)
+        {
+            int evenSum = 0;
+
+            while (num > 0)
+            {
+                int lastDigit = num % 10;
+                num = num / 10;
+
+                if (lastDigit % 2 == 0)
+                {
+                    evenSum += lastDigit;
+                }
+            }
+            return evenSum;
+        }
+
+        private static int GetMultipleOfEvenAndOdds(int num)
+        {
+            int evenSum = GetSumOfEvenDigits(num);
+            int oddSum = GetSumOfOddDigits(num);
+            return evenSum * oddSum;
         }
     }
 }
---
+++ Methods_LabNEx_MathOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Methods_LabNEx_MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Solve(firstNum,operation,secondNum);
+        }
+
+        private static void Solve(int firstNum,char oper, int secondNum)
+        {
+            switch (oper)
+            {
+                case '/':
+                    Console.WriteLine(firstNum/secondNum);
+                    break;
+                case '*':
+                    Console.WriteLine(firstNum*secondNum);
+                    break;
+                case '+':
+                    Console.WriteLine(firstNum+secondNum);
+                    break;
+                case '-':
+                    Console.WriteLine(firstNum-secondNum);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
